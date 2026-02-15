55a105025965aff4d12f5eb837ca51aca47a5b51
ZakNoel <softdeveleng@gmail.com>
2026-02-15 12:40:34 +0500

    Add optional key parameter to dump and dd methods in TelegraphResponse

--- src/Client/TelegraphResponse.php
+++ src/Client/TelegraphResponse.php
@@ -35,7 +35,7 @@ class TelegraphResponse extends Response
         return (int)$this->json('result.message_id');
     }
 
-    public function dump()
+    public function dump($key = null)
     {
         dump($this->json());
 
@@ -43,9 +43,10 @@ class TelegraphResponse extends Response
     }
 
     /**
+     * @param  null  $key
      * @return never-returns
      */
-    public function dd(): void
+    public function dd($key = null): void
     {
         dd($this->json());
     }
